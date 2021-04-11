4692467901503760dd0c87e83f0dd1baf9a39867
Yuki Kodama <endflow.net@gmail.com>
2021-04-11 11:08:43 +0900

    Show result

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { get_owner, get_repo_name, get_pr_number } from "./lib/github";
 const debug = require("debug")("kuy:reporter:github");
 
 export default createReporterFactory(() => async (runner) => {
+  debug("started");
+
   const octokit = new Octokit({
     auth: process.env.GITHUB_TOKEN,
   });
@@ -18,18 +20,36 @@ export default createReporterFactory(() => async (runner) => {
     "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
     {
       ...opts,
-      body: "acot STARTED",
+      body: `Audit by acot (core: ${runner.version.core}, runner: ${runner.version.self})`,
     }
   );
 
-  runner.on("audit:complete", async ([_summary]) => {
-    debug("finished");
-
+  runner.on("audit:complete", async ([summary]) => {
+    // Summarize by paths
+    let body = summary.results
+      .map((result) => {
+        return `${result.url}:  :white_check_mark: ${result.passCount}  :x: ${result.errorCount}  :warning: ${result.warningCount}`;
+      })
+      .join("\n");
     await octokit.request(
       "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
       {
         ...opts,
-        body: "acot FINISHED",
+        body,
+      }
+    );
+
+    // Summarize by rules
+    body = Object.entries(summary.rules)
+      .map(([name, rule]) => {
+        return `${name}:  :white_check_mark: ${rule.passCount}  :x: ${rule.errorCount}  :warning: ${rule.warningCount}`;
+      })
+      .join("\n");
+    await octokit.request(
+      "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+      {
+        ...opts,
+        body,
       }
     );
   });
